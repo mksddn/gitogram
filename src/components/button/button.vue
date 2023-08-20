3ff3d81304ce67a3012fc144981130766b98295e
madu <mr.madu@ya.ru>
2023-08-20 22:35:06 +0300

    Add Button & ProgressBar & SliderItem

---
+++ src/components/button/button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button :class="[
+    'c-button', 'theme-green'
+  ]">
+    <span class="btn-text">
+      {{ text }}
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'button',
+  props: {
+    text: {
+      type: String,
+      default: 'Follow'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./button.scss"></style>
---
+++ src/components/slide/slide.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    <progress-bar />
+    <div class="avatar-container">
+      <person avatarImgSrc="https://placehold.co/300x300" userName="joshua_l" />
+    </div>
+    <div class="content-wrapper">
+      <slot />
+    </div>
+    <xButton />
+  </div>
+</template>
+
+<script>
+import { button } from '../button'
+import { person } from '../person'
+import { progressBar } from '../progress'
+
+export default {
+  name: 'slide',
+  components: {
+    xButton: button,
+    person,
+    progressBar
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./slide.scss"></style>
